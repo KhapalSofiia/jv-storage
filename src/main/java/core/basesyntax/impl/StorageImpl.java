df769ce68d32165309b368ad3ae7e65c7ac090c2
sofiia <sofiia.khapal@nure.ua>
2025-09-07 16:39:02 +0200

    solution

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -22,8 +22,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
                     cargos[i] = new Cargo(key, value);
                     return;
                 }
-                System.out.println("The storage is full!");
             }
+            throw new IllegalStateException("Storage is full");
         }
     }
 
